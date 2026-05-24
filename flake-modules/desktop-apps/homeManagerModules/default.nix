ebdf3914764d9361d58f7cabed50aa1509987416
Keith <kmarcusxiii@gmail.com>
2026-05-24 23:02:12 +0800

    🔧 chore(gitignore): add nix configuration files to ignore list
    - ignore nix.conf and .conf files under nix.conf.d to prevent committing local config
    
    ♻️ refactor(desktop-apps): use unstable packages for desktop applications
    
    - switch from stable pkgs to pkgs-unstable to access newer versions of iina, moonlight-qt, and wechat

--- flake-modules/desktop-apps/homeManagerModules/default.nix
+++ flake-modules/desktop-apps/homeManagerModules/default.nix
@@ -1,7 +1,7 @@
 perSystem:
-{ lib, config, pkgs, ... }:
+{ lib, config, pkgs, pkgs-unstable, ... }:
 {
-  home.packages = with pkgs; [
+  home.packages = with pkgs-unstable; [
     iina              # Modern media player
     moonlight-qt      # NVIDIA GameStream / Sunshine client
     wechat            # WeChat desktop client
